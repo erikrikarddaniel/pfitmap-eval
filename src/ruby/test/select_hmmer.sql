76bfa2d7deca4ab2c38c384ce6239d7bc5cc60a5
Daniel Lundin <matricaria.suaveolens@gmail.com>
2015-11-19 17:24:24 +0100

    Import of basic rows work

--- src/ruby/test/select_hmmer.sql
+++ src/ruby/test/select_hmmer.sql
@@ -13,3 +13,16 @@ FROM
 ORDER BY
   1, 2, 3
 ;
+
+SELECT
+  hp.name, hrr.tname, hrr.qname,
+  hrr.e_value, hrr.score, hrr.bias,
+  hrr.dom_n_exp, hrr.dom_n_reg, hrr.dom_n_clu, hrr.dom_n_ov,
+  hrr.dom_n_env, hrr.dom_n_dom, hrr.dom_n_rep, hrr.dom_n_rep
+FROM
+  hmm_profiles hp JOIN
+  hmm_results hr ON hp.id = hr.hmm_profile_id JOIN
+  hmm_result_rows hrr ON hr.id = hrr.hmm_result_id
+ORDER BY
+  hp.name, hrr.tname, hrr.qname
+;
