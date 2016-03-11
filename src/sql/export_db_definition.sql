f98e4e371ee05e14e83004ad98ea14756444f0bd
Daniel Lundin <daniel.lundin@scilifelab.se>
2016-03-11 09:52:28 +0100

    Modified dump script so that it splits the schema into pre and post files

---
+++ src/sql/export_db_definition.sql
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+targetdir=`dirname $0`
+tmpfile=/tmp/pfitmap-eval.schema.sql
+prefile=$targetdir/pfitmap-eval.schema_pre.sql
+postfile=$targetdir/pfitmap-eval.schema_post.sql
+
+pg_dump -s $DBDEV > $tmpfile
+
+from=`grep -n CONSTRAINT $tmpfile|sed 's/:.*//'|sort -n|head -n 1`
+lines=`wc -l $tmpfile|sed 's/ .*//'`
+
+echo "\$from: $from, \$lines: $lines"
+
+head -n $((from - 2)) $tmpfile > $prefile
+tail -n $((lines - from + 2)) $tmpfile > $postfile
