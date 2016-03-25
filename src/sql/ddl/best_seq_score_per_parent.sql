b00453aeaac9da275e9543744c47507898f0aca5
Daniel Lundin <matricaria.suaveolens@gmail.com>
2016-03-25 02:00:13 +0100

    Forgot to group by parent_id in best scores insert statement

--- src/sql/ddl/best_seq_score_per_parent.sql
+++ src/sql/ddl/best_seq_score_per_parent.sql
@@ -1,16 +1,18 @@
 -- Selects the best scoring child for each parent_id from the seq_scores view.
 
-DROP TABLE best_seq_score_per_parent;
+--DROP TABLE best_seq_score_per_parent;
+TRUNCATE TABLE best_seq_score_per_parent;
 
-CREATE TABLE best_seq_score_per_parent AS
+--CREATE TABLE best_seq_score_per_parent AS
+INSERT INTO best_seq_score_per_parent 
   SELECT 
     ss.*
   FROM 
     seq_scores ss JOIN
     (
-      SELECT sequence_id, sequence_source_id, MAX(score) score
+      SELECT sequence_id, sequence_source_id, parent_id, MAX(score) score
       FROM seq_scores
-      GROUP BY 1, 2
+      GROUP BY 1, 2, 3
     ) ssm ON 
       ss.sequence_id = ssm.sequence_id AND
       ss.sequence_source_id = ssm.sequence_source_id AND
