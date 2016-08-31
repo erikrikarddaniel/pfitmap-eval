450b1f0d8d06500275f57a9cc24fe42c40abd384
Daniel Lundin <matricaria.suaveolens@gmail.com>
2016-08-31 16:10:12 +0200

    Starting on rmarkdown for consistency checks.

--- src/R/classified_proteins_ge_0.9_to_feather.r
+++ src/R/classified_proteins_ge_0.9_to_feather.r
@@ -5,10 +5,14 @@
 
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(feather))
+suppressPackageStartupMessages(library(readr))
 
 # Connect and define table source
 db = src_postgres('pfitmap-eval-prod')
 
-cp = tbl(db, 'classified_proteins')
+cp = collect(tbl(db, 'classified_proteins') %>% filter(prop_matching>=0.9), n=Inf)
 
-write_feather(collect(cp %>% filter(prop_matching>=0.9), n=Inf), 'classified_proteins.prop_matching_ge_0.9.feather')
+write_feather(cp, 'classified_proteins.prop_matching_ge_0.9.feather')
+
+# Since feather is a bit unstable, also write to tsv
+write_tsv(cp, 'classified_proteins.prop_matching_ge_0.9.tsv')
