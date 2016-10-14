ddf4229a59ac66874d955d0d09e3d8445d2b77af
Daniel Lundin <matricaria.suaveolens@gmail.com>
2016-10-14 09:51:14 +0200

    Changed so that alignment lengths are calculated as the sum of all individual subalignments

--- src/sql/ddl/align_lengths.sql
+++ src/sql/ddl/align_lengths.sql
@@ -3,9 +3,16 @@ CREATE OR REPLACE VIEW align_lengths AS
     hmm_result_row_id,
     MIN(hmm_from) min_hmm_from,
     MAX(hmm_to) max_hmm_to,
-    MAX(hmm_to) - MIN(hmm_from) + 1 length
-  FROM
-    hmm_result_domains
+    SUM(length) length
+  FROM (
+    SELECT
+      hmm_result_row_id,
+      hmm_from,
+      hmm_to,
+      hmm_to - hmm_from + 1 length
+    FROM
+      hmm_result_domains
+  ) a
   GROUP BY
     hmm_result_row_id
 ;
