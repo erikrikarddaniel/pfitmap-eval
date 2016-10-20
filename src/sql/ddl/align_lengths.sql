68e963b8b5ef5386490d88ee6c49cebe55b8b5c9
Daniel Lundin <erik.rikard.daniel@gmail.com>
2016-10-20 22:22:39 +0200

    Added ali_from and ali_to to align_lengths view.

--- src/sql/ddl/align_lengths.sql
+++ src/sql/ddl/align_lengths.sql
@@ -3,12 +3,16 @@ CREATE OR REPLACE VIEW align_lengths AS
     hmm_result_row_id,
     MIN(hmm_from) min_hmm_from,
     MAX(hmm_to) max_hmm_to,
+    MIN(ali_from) min_ali_from,
+    MAX(ali_to) max_ali_to,
     SUM(length) length
   FROM (
     SELECT
       hmm_result_row_id,
       hmm_from,
       hmm_to,
+      ali_from,
+      ali_to,
       hmm_to - hmm_from + 1 length
     FROM
       hmm_result_domains
