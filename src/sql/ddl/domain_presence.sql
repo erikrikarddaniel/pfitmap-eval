b405eebc404e191841854341f58667fdd431468d
Daniel Lundin <erik.rikard.daniel@gmail.com>
2016-10-20 22:03:02 +0200

    Table creation script for domain_presence, with two new columns and primary key

---
+++ src/sql/ddl/domain_presence.sql
@@ -0,0 +1,32 @@
+/**
+ * Creates the domain_presence table.
+ *
+ * Author: daniel.lundin@dbb.su.se
+ */
+
+CREATE TABLE domain_presence (
+  seq_src		TEXT	NOT NULL,
+  db			TEXT	NOT NULL,
+  accno			TEXT	NOT NULL,
+  domain		TEXT	NOT NULL,
+  profile_length	TEXT	NOT NULL,
+  align_length		INT	NOT NULL,
+  align_from		INT	NOT NULL,
+  align_to		INT	NOT NULL,
+  prop_matching		FLOAT	NOT NULL,
+  score			FLOAT	NOT NULL,
+  ss_source		TEXT	NOT NULL,
+  ss_name		TEXT	NOT NULL,
+  ss_version		TEXT	NOT NULL
+);
+
+ALTER TABLE domain_presence
+  ADD PRIMARY KEY (domain, ss_source, ss_name, ss_version, seq_src, accno)
+;
+
+ALTER TABLE domain_presence
+  ADD COLUMN
+    profile_from	INT,
+  ADD COLUMN
+    profile_to		INT
+;
