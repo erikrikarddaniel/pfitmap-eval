c57f8a4e0cac004d485e5d577545ebb2096af502
Daniel Lundin <erik.rikard.daniel@gmail.com>
2018-02-01 16:40:07 +0100

    Include domains

--- src/sql/queries/hmm_profile_hierarchy.sql
+++ src/sql/queries/hmm_profile_hierarchy.sql
@@ -12,4 +12,19 @@ FROM
   hmm_profiles hp JOIN
   hmm_profile_hierarchies hpp ON
   hp.id = hpp.hmm_profile_id
+UNION
+  SELECT
+    hp.name profile,
+    hp.rank rank,
+    NULL AS superfamily,
+    NULL AS family,
+    NULL AS class,
+    NULL AS subclass,
+    NULL AS group,
+    hp.version,
+    hp.length
+  FROM
+    hmm_profiles hp
+  WHERE
+    rank = 'domain'
 ;
